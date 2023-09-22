296beddd543972e46c6eb83b201c3170882bc8e7
Alessandro Mazzon <alessandro@forbole.com>
2023-09-22 16:01:44 +0200

    fix: generated a storage password

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,6 @@
 namespace NodeJS {
   interface ProcessEnv {
-    NEXT_PUBLIC_FORBOLE_GQL: string;
-    NEXT_PUBLIC_DESMOS_GQL: string;
-    NEXT_PUBLIC_BONDSCAPE_GQL: string;
-    NEXT_PUBLIC_BONDSCAPE_REST: string;
+    NEXT_PUBLIC_COOKIES_STORAGE_PASSWORD: string;
     NEXT_PUBLIC_GOOGLE_PLACES_API_KEY: string;
     NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID_MAINNET: string;
   }
