ecdc02fe0fddba078f85aada11c072e3bbefd20b
Alessandro Mazzon <alessandro@forbole.com>
2023-09-30 10:12:46 +0200

    feat: replaced date time picker with a better one

--- app/components/BondscapeWrapper.tsx
+++ app/components/BondscapeWrapper.tsx
@@ -3,20 +3,23 @@ import React from "react";
 import { useInitWeb3Auth } from "@/hooks/web3Auth/useInitWeb3Auth";
 import { SWRConfig } from "swr";
 import fetchJson from "@/lib/fetchJson";
+import StyledComponentsRegistry from "@/components/AntdRegistry";
 
 const BondscapeWrapper = ({ children }: React.PropsWithChildren) => {
   useInitWeb3Auth();
   return (
-    <SWRConfig
-      value={{
-        fetcher: fetchJson,
-        onError: (err) => {
-          console.error(err);
-        },
-      }}
-    >
-      {children}
-    </SWRConfig>
+    <StyledComponentsRegistry>
+      <SWRConfig
+        value={{
+          fetcher: fetchJson,
+          onError: (err) => {
+            console.error(err);
+          },
+        }}
+      >
+        {children}
+      </SWRConfig>
+    </StyledComponentsRegistry>
   );
 };
 
--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -1,7 +1,3 @@
-import { zonedTimeToUtc } from "date-fns-tz";
-// @ts-ignore
-import { IDay } from "react-calendar-datetime-picker/dist/types/type";
-
 export function escapeChars(htmlStr: string) {
   return htmlStr
     .replace(/&/g, "&amp;")
@@ -10,18 +6,3 @@ export function escapeChars(htmlStr: string) {
     .replace(/"/g, "&quot;")
     .replace(/'/g, "&#39;");
 }
-
-export const datePickerToUTCDatetime = (date: IDay) => {
-  if (!date) return undefined;
-  const localeDate = new Date(
-    date.year,
-    date.month,
-    date.day,
-    date.hour,
-    date.minute,
-  );
-  return zonedTimeToUtc(
-    localeDate,
-    Intl.DateTimeFormat().resolvedOptions().timeZone,
-  ).toISOString();
-};
